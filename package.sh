b10c19ff452602015e52e43d98e2f29534ff74c0
kongra <kongra@gmail.com>
2021-04-30 13:50:46 +0200

    We are now a Jakarta EE project

---
+++ package.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+mvn clean package -Dtest=!koJ.JmhRunnerTest
---
+++ src/main/java/telsos/Main.java
@@ -0,0 +1,9 @@
+package telsos;
+
+public class Main {
+
+  public static void main(String... args) {
+    // TODO: implement
+  }
+
+}
