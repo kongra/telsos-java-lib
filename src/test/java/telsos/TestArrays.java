c197f927dabf9a060a489a452fde66436ddcd26e
kongra <kongra@gmail.com>
2021-05-14 16:36:05 +0200

    Test added

--- src/test/java/telsos/TestArrays.java
+++ src/test/java/telsos/TestArrays.java
@@ -4,13 +4,22 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 import org.junit.jupiter.api.Test;
 
+import telsos.db.MAAS;
+
 class TestArrays {
 
   @Test
-  void test() {
+  void test1() {
     assertThat(2 <= 2).isTrue();
     assertThat(2 <= 20).isTrue();
     assertThat(2 <= 1).isFalse();
+  }
+
+  @Test
+  void test2() {
+    MAAS.get().inReadCommitted(ctx -> {
+      System.out.println("It works");
+    });
   }
 
 }
