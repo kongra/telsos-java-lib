c1f5e701d50f9fd93a8e6206123b5c86fe0c8a13
kongra <kongra@gmail.com>
2021-05-17 19:16:00 +0200

    Update

--- src/main/java/telsos/db/MAAS.java
+++ src/main/java/telsos/db/MAAS.java
@@ -1,13 +1,15 @@
 package telsos.db;
 
+import javax.sql.DataSource;
+
 import org.jooq.SQLDialect;
 
 import com.zaxxer.hikari.HikariConfig;
 
-public class MAAS extends HikariPool implements DBI {
+public class MAAS implements DBI {
 
   public static MAAS get() {
-    return INSTANCE;
+    return instance;
   }
 
   @Override
@@ -16,23 +18,30 @@ public class MAAS extends HikariPool implements DBI {
   }
 
   @Override
-  public HikariConfig hikariConfig() {
-    var config = new HikariConfig();
-    config.setJdbcUrl("jdbc:postgresql://localhost/MAAS");
-    config.setUsername("jee");
-    config.setPassword("jee");
-    config.addDataSourceProperty("cachePrepStmts", "true");
-    config.addDataSourceProperty("prepStmtCacheSize", "250");
-    config.addDataSourceProperty("prepStmtCacheSqlLimit", "2048");
-
-    config.setMaximumPoolSize(100);
-    config.setAutoCommit(true);
-    config.setValidationTimeout(5000);
-
-    return config;
+  public DataSource dataSource() {
+    return pool.dataSource();
   }
 
-  private static final MAAS INSTANCE = new MAAS();
+  private final HikariPool pool = new HikariPool() {
+    @Override
+    public HikariConfig hikariConfig() {
+      var config = new HikariConfig();
+      config.setJdbcUrl("jdbc:postgresql://localhost/MAAS");
+      config.setUsername("jee");
+      config.setPassword("jee");
+      config.addDataSourceProperty("cachePrepStmts", "true");
+      config.addDataSourceProperty("prepStmtCacheSize", "250");
+      config.addDataSourceProperty("prepStmtCacheSqlLimit", "2048");
+
+      config.setMaximumPoolSize(100);
+      config.setAutoCommit(true);
+      config.setValidationTimeout(5000);
+
+      return config;
+    }
+  };
+
+  private static final MAAS instance = new MAAS();
 
   private MAAS() {
   }
