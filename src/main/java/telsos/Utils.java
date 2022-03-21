5b749bc19de1bdaccf5da5db9358558750a8d1e6
kongra <kongra@gmail.com>
2022-03-21 16:54:54 +0100

    Cleanup

--- src/main/java/telsos/Utils.java
+++ src/main/java/telsos/Utils.java
@@ -2,17 +2,6 @@
 // Created 18.07.19
 package telsos;
 
-import java.io.IOException;
-
-import com.fasterxml.jackson.core.JsonProcessingException;
-
-import jakarta.servlet.ServletException;
-import jakarta.servlet.http.HttpServletRequest;
-import jakarta.servlet.http.HttpServletResponse;
-import jakarta.ws.rs.core.Response;
-import jakarta.ws.rs.core.Response.Status;
-import telsos.function.ThrowingSupplier;
-
 public final class Utils {
 
   /**
@@ -32,27 +21,6 @@ public final class Utils {
   @SuppressWarnings("unchecked")
   private static <T extends Throwable> void sneakyThrow0(Throwable t) throws T {
     throw (T) t;
-  }
-
-  public static Response forward(String path, HttpServletRequest request,
-      HttpServletResponse response) {
-    try {
-      request.getRequestDispatcher(path).forward(request, response);
-      return null;
-    } catch (ServletException | IOException e) {
-      return Response.status(Status.INTERNAL_SERVER_ERROR).build();
-    }
-  }
-
-  public static Response endPoint(
-      ThrowingSupplier<Response, JsonProcessingException> supplier) {
-    try {
-      return supplier.get();
-    } catch (JsonProcessingException e) {
-      return Response.status(Status.BAD_REQUEST).build();
-    } catch (TelsosException | ChError e) {
-      return Response.status(Status.INTERNAL_SERVER_ERROR).build();
-    }
   }
 
   private Utils() {
