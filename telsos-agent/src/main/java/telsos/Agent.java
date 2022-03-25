23aa872707f12f748d4c58a44d400f40396d1d5b
kongra <kongra@gmail.com>
2022-03-25 10:39:35 +0100

    Formatting

---
+++ telsos-agent/src/main/java/telsos/Agent.java
@@ -0,0 +1,32 @@
+package telsos;
+
+import java.lang.instrument.Instrumentation;
+import java.util.concurrent.atomic.AtomicReference;
+
+public class Agent {
+
+  public static Instrumentation instrumentation() {
+    return premained.get().instr();
+  }
+
+  public static void premain(String agentArgs, Instrumentation instr) {
+    System.out.println("telsos.Agent::premain");
+    init(agentArgs, instr);
+  }
+
+  public static void agentmain(String agentArgs, Instrumentation instr) {
+    System.out.println("telsos.Agent::agentmain");
+    init(agentArgs, instr);
+  }
+
+  private static void init(String agentArgs, Instrumentation instr) {
+    premained.set(new Premained(instr, agentArgs));
+  }
+
+  private static record Premained(Instrumentation instr, String agentArgs) {}
+
+  private static final AtomicReference<Premained> premained = new AtomicReference<>();
+
+  private Agent() {}
+
+}
