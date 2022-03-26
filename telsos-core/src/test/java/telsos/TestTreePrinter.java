747ba3df0872f49b8b9ccd176e87cd14ca9d2600
kongra <kongra@gmail.com>
2022-03-26 21:51:46 +0100

    Cleanup

--- telsos-core/src/test/java/telsos/TestTreePrinter.java
+++ telsos-core/src/test/java/telsos/TestTreePrinter.java
@@ -1,19 +1,3 @@
 package telsos;
 
-import java.util.List;
-
-import org.junit.jupiter.api.Test;
-
-import telsos.pprint.TreePrinter;
-
-class TestTreePrinter {
-
-  @Test
-  void test() {
-    TreePrinter<Integer> printer = TreePrinter.of(i -> List.of(i + 1, i + 2),
-        String::valueOf);
-
-    var depth = 2;
-    printer.pprint(0, depth, s -> System.out.print(s));
-  }
-}
+class TestTreePrinter {}
