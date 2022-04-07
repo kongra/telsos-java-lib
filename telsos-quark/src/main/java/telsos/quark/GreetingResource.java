cf14a84a6e25409362b36a3ce7f9a904e0315bb2
kongra <kongra@gmail.com>
2022-04-07 18:30:02 +0200

    Update

--- telsos-quark/src/main/java/telsos/quark/GreetingResource.java
+++ telsos-quark/src/main/java/telsos/quark/GreetingResource.java
@@ -1,16 +1,28 @@
 package telsos.quark;
 
+import javax.inject.Inject;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 
-@Path("/hello")
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+@Path("/greeting")
 public class GreetingResource {
 
+  @Inject
+  GreetingTools greetingTools;
+
   @GET
+  @Path("/hello")
   @Produces(MediaType.TEXT_PLAIN)
   public String hello() {
-    return "Hello RESTEasy";
+    LOG.debug("Is's ok to be nice");
+    return greetingTools.sayHello("Test");
   }
+
+  private static final Logger LOG = LoggerFactory
+      .getLogger(GreetingResource.class);
 }
