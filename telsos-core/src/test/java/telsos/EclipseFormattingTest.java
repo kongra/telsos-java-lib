a09103aeb9774f9334ec391fbbcf635f742e89c2
kongra <kongra@gmail.com>
2022-07-23 23:18:27 +0200

    Refactoring

--- telsos-core/src/test/java/telsos/EclipseFormattingTest.java
+++ telsos-core/src/test/java/telsos/EclipseFormattingTest.java
@@ -16,10 +16,29 @@ class EclipseFormattingTest {
 
   @Test
   void testFormatting() {
-    var list1 = List.of(1, 2, 3, 4, 5, 6, 7, 8);
-    var list2 = list1.stream().filter(this::isOdd).collect(Collectors.toList());
+    final var list1 = List.of(1, 2, 3, 4, 5, 6, 7, 8);
+    final var list2 = list1
+        .stream()
+        .filter(this::isOdd)
+        .collect(Collectors.toList());
 
-    assertThat(list2).isNotEmpty();
+    final var s1 = """
+        some longer text
+        that contains just
+        a few random lines
+        """;
+
+    final var l1 = s1.lines()
+        .map(l -> l + " ")
+        .map(l -> l + "-")
+        .map(l -> l + "/")
+        .map(l -> l + "|" + '\n')
+        .toList();
+
+    System.out.println(s1);
+    System.out.println(l1);
+
+    assertThat(list2).hasSize(4);
   }
 
 }
