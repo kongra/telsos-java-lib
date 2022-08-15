472b52ee224ec95dd9b9b671c749b1f44107ef6f
kongra <kongra@gmail.com>
2022-08-15 15:43:21 +0200

    Update.

---
+++ telsos-quark/src/test/java/telsos/quark/PostgresDataSourceTest.java
@@ -0,0 +1,19 @@
+package telsos.quark;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+class PostgresDataSourceTest {
+
+  @BeforeEach
+  void setUp() throws Exception {}
+
+  @Test
+  void testOf() {
+    final var sample = 0;
+    assertThat(sample).isZero();
+  }
+
+}
