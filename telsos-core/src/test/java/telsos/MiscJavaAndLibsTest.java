06ab67e0bf2f411c7b3d6a738af85390d43f726f
kongra <kongra@gmail.com>
2022-08-19 22:05:41 +0200

    Refactoring, added some simple tests.

--- telsos-core/src/test/java/telsos/MiscJavaAndLibsTest.java
+++ telsos-core/src/test/java/telsos/MiscJavaAndLibsTest.java
@@ -5,24 +5,27 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 import org.eclipse.collections.api.factory.Lists;
 import org.eclipse.collections.api.list.MutableList;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-class EclipseCollectionsTest {
-
-  MutableList<Integer> mutableList;
-
-  @BeforeEach
-  void setupEach() {
-    mutableList = Lists.mutable.of(1, 2, 3);
-  }
+@SuppressWarnings("static-method")
+class MiscJavaAndLibsTest {
 
   @Test
   void testMutableListConsistency() {
+    final MutableList<Integer> mutableList = Lists.mutable.of(1, 2, 3);
     assertThat(mutableList)
         .contains(1)
         .contains(2)
         .contains(3);
+  }
+
+  @Test
+  void testNaN2IntConversion() {
+    final var x = Math.sqrt(-5);
+    assertThat(x).isNaN();
+
+    final var n = (int) x;
+    assertThat(n).isZero();
   }
 
 }
