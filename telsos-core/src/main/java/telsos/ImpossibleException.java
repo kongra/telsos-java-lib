8c94364cb705df30afc5c00fed6eb194e59c7138
kongra <kongra@gmail.com>
2022-09-15 17:08:50 +0200

    Refactoring.

--- telsos-core/src/main/java/telsos/ImpossibleException.java
+++ telsos-core/src/main/java/telsos/ImpossibleException.java
@@ -4,21 +4,21 @@ public class ImpossibleException extends RuntimeException {
 
   public ImpossibleException() {}
 
-  public ImpossibleException(String message, Throwable cause,
-      boolean enableSuppression, boolean writableStackTrace) {
-    super(message, cause, enableSuppression, writableStackTrace);
-  }
-
-  public ImpossibleException(String message, Throwable cause) {
-    super(message, cause);
-  }
-
   public ImpossibleException(String message) {
     super(message);
   }
 
   public ImpossibleException(Throwable cause) {
     super(cause);
+  }
+
+  public ImpossibleException(String message, Throwable cause) {
+    super(message, cause);
+  }
+
+  public ImpossibleException(String message, Throwable cause,
+      boolean enableSuppression, boolean writableStackTrace) {
+    super(message, cause, enableSuppression, writableStackTrace);
   }
 
 }
