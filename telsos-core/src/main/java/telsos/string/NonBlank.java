36e6ed8fd41cceba6f4ae73746378e17cf5b8806
kongra <kongra@gmail.com>
2022-12-11 23:56:08 +0100

    Re-design.

--- telsos-core/src/main/java/telsos/string/NonBlank.java
+++ telsos-core/src/main/java/telsos/string/NonBlank.java
@@ -4,17 +4,23 @@ import java.util.Optional;
 
 import telsos.newtype.Newtype;
 
-public final class NonBlank extends Newtype<String> {
+public class NonBlank extends Newtype<String> {
 
   public static Optional<NonBlank> of(String s) {
     return of(s, NonBlank::isNonBlank, NonBlank::new);
+  }
+
+  public static Optional<NonBlank> ofTrimmed(String s) {
+    return Optional.ofNullable(s)
+        .map(String::trim)
+        .flatMap(NonBlank::of);
   }
 
   public static boolean isNonBlank(String s) {
     return s != null && !s.isBlank();
   }
 
-  private NonBlank(String value) {
+  protected NonBlank(String value) {
     super(value);
   }
 
