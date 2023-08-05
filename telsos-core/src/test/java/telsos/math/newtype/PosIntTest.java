b09cdb5cfd90ee6109adc36cbc89d86701c90e3b
Konrad Grzanek <kongra@gmail.com>
2023-08-05 19:25:10 +0200

    Update

---
+++ telsos-core/src/test/java/telsos/math/newtype/PosIntTest.java
@@ -0,0 +1,35 @@
+// © 2023 Konrad Grzanek <kongra@gmail.com>
+package telsos.math.newtype;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class PosIntTest {
+
+  @Test
+  void testOfInt() {
+    fail("Not yet implemented");
+  }
+
+  @Test
+  void testOfNullableInteger() {
+    fail("Not yet implemented");
+  }
+
+  @Test
+  void testIsPos() {
+    fail("Not yet implemented");
+  }
+
+  @Test
+  void testHashCode() {
+    fail("Not yet implemented");
+  }
+
+  @Test
+  void testEqualsObject() {
+    fail("Not yet implemented");
+  }
+
+}
