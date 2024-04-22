b660b2973a908930252a6c95be5e686fa35a5f9d
Konrad Grzanek <kongra@gmail.com>
2024-04-22 12:43:38 +0200

    Created ByteConsumer

---
+++ telsos-core/src/main/java/telsos/function/ByteConsumer.java
@@ -0,0 +1,19 @@
+// © 2024 Konrad Grzanek <kongra@gmail.com>
+package telsos.function;
+
+import java.util.Objects;
+
+@FunctionalInterface
+public interface ByteConsumer {
+
+  void accept(byte value);
+
+  default ByteConsumer andThen(ByteConsumer after) {
+    Objects.requireNonNull(after);
+    return t -> {
+      accept(t);
+      after.accept(t);
+    };
+  }
+
+}
